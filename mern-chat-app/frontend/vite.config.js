c95f023d8af6a746d7443145fe3028067ccea391
Soni Darshan <soni.darshan0209@gmail.com>
2024-12-16 23:53:05 +0530

    d

--- mern-chat-app/frontend/vite.config.js
+++ mern-chat-app/frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), require("tailwindcss"), require("autoprefixer")],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
