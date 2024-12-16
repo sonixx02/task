a2f6e6781ad075f33e6ac452f446fa2ab04759fd
Soni Darshan <soni.darshan0209@gmail.com>
2024-12-17 00:16:09 +0530

    done

--- mern-chat-app/frontend/src/components/Admin/AdminDashboard.jsx
+++ mern-chat-app/frontend/src/components/Admin/AdminDashboard.jsx
@@ -7,7 +7,7 @@ const AdminDashboard = () => {
   const [newUser, setNewUser] = useState({ name: '', email: '', mobileNo: '', password: '' });
   const [file, setFile] = useState(null);
   const navigate = useNavigate();
-  const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+  const backendUrl = import.meta.env.VITE_BACKEND_URL ;
   useEffect(() => {
     
     const token = localStorage.getItem('token');
